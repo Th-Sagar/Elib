907503699280247184801ed2c0bb372d2b07d22f
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-15 07:54:59 +0545

    chores:setup express

--- server.ts
+++ server.ts
@@ -1,1 +1,10 @@
-console.log("welcome to ebook server")
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+startServer();
