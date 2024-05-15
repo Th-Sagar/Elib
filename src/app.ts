ec384c11e369ba299028e7d78d737393163d3494
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-15 09:12:54 +0545

    feat: global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
   res.json({ message: "Hello World" });
 });
+
+//global error handler
+
+app.use(globalErrorHandler);
 
 export default app;
