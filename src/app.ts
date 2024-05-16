5b9f7863272c335ae878ba291870877ac9ea822a
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-16 07:30:41 +0545

    feat: user router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
   res.json({ message: "Hello World" });
 });
+
+app.use("/api/users", userRouter);
 
 //global error handler
 
