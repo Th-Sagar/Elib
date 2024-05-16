4cf23f3907e12e4fb3d2e2ffa9415d6ba50a651a
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-16 07:54:57 +0545

    feat: user model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import User from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -9,6 +10,12 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       400,
       "Please provide all the required fields"
     );
+    return next(error);
+  }
+
+  const user = await User.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "User already exists with this email");
     return next(error);
   }
 
