2f501f7cd3f57a17d9597f03ebf3bdc850a4c725
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-16 07:41:38 +0545

    feat:validate data

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,17 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    const error = createHttpError(
+      400,
+      "Please provide all the required fields"
+    );
+    return next(error);
+  }
+
   res.json({ message: "User created successfully" });
 };
 
