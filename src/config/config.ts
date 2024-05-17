904a7b48ccd2fb8bb45cc9418f5d6a225b680c3f
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-17 22:27:55 +0545

    feat:cloudinary

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,9 @@ const _config = {
   databaseUrl: process.env.MONGO_CONNECTION_STRING,
   env: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECRET,
+  cloudinaryCloud: process.env.ClOUDINARY_CLOUD,
+  cloudinaryApiKey: process.env.ClOUDINARY_API_KEY,
+  cloudinaryApiSecret: process.env.ClOUDINARY_API_SECRET,
 };
 
 export const config = Object.freeze(_config);
