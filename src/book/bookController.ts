2d4249c7b4719138cc2ccfca7f038814eaa3c711
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-17 17:15:44 +0545

    feat:multer

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 const createBook = (req: Request, res: Response, next: NextFunction) => {
+  const {} = req.body;
   res.json({ message: "Book created successfully" });
 };
 
